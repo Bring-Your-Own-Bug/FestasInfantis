6e93ad66aaa039e732151f6e16c3c7f37aa21a57
Rafael Santos <rafaelsantos138@hotmail.com>
2023-06-13 14:58:01 -0300

    Corrigido nome das propriedades booleanas do controlador base

--- FestasInfantis.WinApp/Compartilhado/ControladorBase.cs
+++ FestasInfantis.WinApp/Compartilhado/ControladorBase.cs
@@ -6,9 +6,9 @@
         public abstract string ToolTipEditar { get; }
         public abstract string ToolTipExcluir { get; }
 
-        public virtual bool ehInserirHabilitado => true;
-        public virtual bool ehEditarHabilitado => true;
-        public virtual bool ehExcluirHabilitado => true;
+        public virtual bool EhInserirHabilitado => true;
+        public virtual bool EhEditarHabilitado => true;
+        public virtual bool EhExcluirHabilitado => true;
 
         public abstract void Inserir();
 
