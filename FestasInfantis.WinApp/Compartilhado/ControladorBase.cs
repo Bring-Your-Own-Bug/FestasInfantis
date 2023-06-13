c04ae9fb147a6bde785574d5048cde3247982448
Rafael Santos <rafaelsantos138@hotmail.com>
2023-06-13 14:50:49 -0300

    Adicionado Pasta Compartilhado

---
+++ FestasInfantis.WinApp/Compartilhado/ControladorBase.cs
@@ -0,0 +1,21 @@
+﻿namespace FestasInfantis.WinApp.Compartilhado
+{
+    public abstract class ControladorBase
+    {
+        public abstract string ToolTipInserir { get; }
+        public abstract string ToolTipEditar { get; }
+        public abstract string ToolTipExcluir { get; }
+
+        public virtual bool ehInserirHabilitado => true;
+        public virtual bool ehEditarHabilitado => true;
+        public virtual bool ehExcluirHabilitado => true;
+
+        public abstract void Inserir();
+
+        public abstract void Editar();
+
+        public abstract void Excluir();
+
+        public abstract string ObterTipoCadastro();
+    }
+}
---
+++ FestasInfantis.WinApp/Compartilhado/EntidadeBase.cs
@@ -0,0 +1,11 @@
+﻿namespace FestasInfantis.WinApp.Compartilhado
+{
+    public abstract class EntidadeBase<T>
+    {
+        public int Id { get; set; }
+
+        public abstract void AtualizarInformacoes(T registroAtualizado);
+
+        public abstract List<string> Validate();
+    }
+}
