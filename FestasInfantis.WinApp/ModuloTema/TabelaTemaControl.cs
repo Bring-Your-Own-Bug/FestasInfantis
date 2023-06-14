90be4f0218f6ad7478b7dbd4d0ea68a82147cc52
Rafael Santos <rafaelsantos138@hotmail.com>
2023-06-14 15:31:11 -0300

    Configurações básicas do grid de tema adicionado

--- FestasInfantis.WinApp/ModuloTema/TabelaTemaControl.cs
+++ FestasInfantis.WinApp/ModuloTema/TabelaTemaControl.cs
@@ -6,7 +6,35 @@
         {
             InitializeComponent();
 
+            ConfigurarColunas()
 
+            grid.ConfigurarGridZebrado();
+
+            grid.ConfigurarGridSomenteLeitura();
+        }
+
+        private void ConfigurarColunas()
+        {
+            DataGridViewTextBoxColumn[] colunas = new DataGridViewTextBoxColumn[]
+            {
+                new()
+                {
+                    Name = "id",
+                    HeaderText = "ID"
+                },
+                new()
+                {
+                    Name = "nome",
+                    HeaderText = "Tema"
+                },
+                new()
+                {
+                    Name = "valor",
+                    HeaderText = "Valor Total do Tema"
+                }
+            };
+
+            grid.Columns.AddRange(colunas);
         }
     }
 }
