f47757c56c57cda048c017a2066788e65c0cc2a1
Rafael Santos <rafaelsantos138@hotmail.com>
2023-06-15 13:18:55 -0300

    Adicionado entidade endereço e validações nas entidades

---
+++ FestasInfantis.Dominio/ModuloFesta/Endereco.cs
@@ -0,0 +1,43 @@
+﻿namespace FestasInfantis.Dominio.ModuloFesta
+{
+    public class Endereco
+    {
+        public string Rua { get; set; }
+        public int Numero { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+
+        public Endereco()
+        {
+           
+        }
+
+        public Endereco(string rua, int numero, string bairro, string cidade, string estado)
+        {
+            Rua = rua;
+            Numero = numero;
+            Bairro = bairro;
+            Cidade = cidade;
+            Estado = estado;
+        }
+
+        public List<string> ValidarEndereco()
+        {
+            List<string> erros = new();
+
+            if (string.IsNullOrWhiteSpace(Rua))
+                erros.Add("O campo 'rua' é obrigatório");
+            if (Numero < 0)
+                erros.Add("O campo 'numero' não pode ser negativo");
+            if (string.IsNullOrWhiteSpace(Bairro))
+                erros.Add("O campo 'bairro' é obrigatório");
+            if (string.IsNullOrWhiteSpace(Cidade))
+                erros.Add("O campo 'cidade' é obrigatório");
+            if (string.IsNullOrWhiteSpace(Estado))
+                erros.Add("O campo 'estado' é obrigatório");
+
+            return erros;
+        }
+    }
+}
--- FestasInfantis.Dominio/ModuloFesta/Festa.cs
+++ FestasInfantis.Dominio/ModuloFesta/Festa.cs
@@ -1,15 +1,37 @@
-﻿namespace FestasInfantis.Dominio.ModuloFesta
+﻿using FestasInfantis.Dominio.ModuloTema;
+
+namespace FestasInfantis.Dominio.ModuloFesta
 {
     public class Festa : EntidadeBase<Festa>
     {
+        public Tema Tema { get; set; }
+        public DateTime Data { get; set; }
+        public TimeSpan HorarioInicio { get; set; }
+        public TimeSpan HorarioFinal { get; set; }
+        public Endereco Endereco { get; set; }
+
         public override void AtualizarInformacoes(Festa registroAtualizado)
         {
-            throw new NotImplementedException();
+            Id = registroAtualizado.Id;
+            Data = registroAtualizado.Data;
+            HorarioInicio = registroAtualizado.HorarioInicio;
+            HorarioFinal = registroAtualizado.HorarioFinal;
+            Endereco = registroAtualizado.Endereco;
         }
 
         public override List<string> Validar()
         {
-            throw new NotImplementedException();
+            List<string> erros = new();
+
+            TimeSpan diferencaHorario = HorarioFinal - HorarioInicio;
+
+            if (diferencaHorario.TotalMinutes < 15)
+                erros.Add("A diferença entre o horário de início e o horário de término deve ser de pelo menos 15 minutos");
+
+            if (HorarioInicio >= HorarioFinal)
+                erros.Add("Horário de início deve ser menor que o horário final");
+
+            return erros;
         }
     }
 }
