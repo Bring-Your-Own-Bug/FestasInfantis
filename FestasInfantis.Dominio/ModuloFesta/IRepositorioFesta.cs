28758b9fe5720e251ae802b13364c9a16460496d
Rafael Santos <rafaelsantos138@hotmail.com>
2023-06-15 13:23:06 -0300

    configurado IRepositorioFesta

--- FestasInfantis.Dominio/ModuloFesta/IRepositorioFesta.cs
+++ FestasInfantis.Dominio/ModuloFesta/IRepositorioFesta.cs
@@ -2,5 +2,10 @@
 {
     public interface IRepositorioFesta
     {
+        void Inserir(Festa festa);
+        void Editar(int id, Festa festa);
+        void Excluir(Festa festa);
+        Festa SelecionarPorId(int id);
+        List<Festa> SelecionarTodos();
     }
 }
