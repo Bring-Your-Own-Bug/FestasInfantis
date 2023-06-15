a67aefc491acef9e9e8748f5956856b6966606ac
Rafael Santos <rafaelsantos138@hotmail.com>
2023-06-15 02:00:48 -0300

    linhas reduzidas

--- FestasInfantis.WinApp/ModuloTema/ControladorTema.cs
+++ FestasInfantis.WinApp/ModuloTema/ControladorTema.cs
@@ -25,13 +25,9 @@ namespace FestasInfantis.WinApp.ModuloTema
         {
             TelaTemaForm telaTema = new();
 
-            DialogResult dialogResult = telaTema.ShowDialog();
-
-            if (dialogResult == DialogResult.OK)
+            if (telaTema.ShowDialog() == DialogResult.OK)
             {
-                Tema tema = telaTema.ObterTema();
-
-                _repositorioTema.Inserir(tema);
+                _repositorioTema.Inserir(telaTema.ObterTema());
             }
 
             CarregarTemas();
@@ -47,20 +43,15 @@ namespace FestasInfantis.WinApp.ModuloTema
                     "Edição de Temas",
                     MessageBoxButtons.OK,
                     MessageBoxIcon.Exclamation);
-
                 return;
             }
 
             TelaTemaForm telaTema = new();
             telaTema.ConfigurarForm(temaSelecionado);
 
-            DialogResult dialogResult = telaTema.ShowDialog();
-
-            if (dialogResult == DialogResult.OK)
+            if (telaTema.ShowDialog() == DialogResult.OK)
             {
-                Tema tema = telaTema.ObterTema();
-
-                _repositorioTema.Editar(tema.Id, tema);
+                _repositorioTema.Editar(telaTema.ObterTema().Id, telaTema.ObterTema());
             }
 
             CarregarTemas();
@@ -76,14 +67,11 @@ namespace FestasInfantis.WinApp.ModuloTema
                     "Exclusão de Temas",
                     MessageBoxButtons.OK,
                     MessageBoxIcon.Exclamation);
-
                 return;
             }
 
-            DialogResult dialogResult = MessageBox.Show($"Deseja excluir o tema {temaSelecionado.Nome}?",
-                    "Exclusão de Temas", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
-
-            if (dialogResult == DialogResult.OK)
+            if (MessageBox.Show($"Deseja excluir o tema {temaSelecionado.Nome}?", "Exclusão de Temas",
+                MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
                 _repositorioTema.Excluir(temaSelecionado);
 
             CarregarTemas();
@@ -99,15 +87,12 @@ namespace FestasInfantis.WinApp.ModuloTema
                     "Itens do Tema",
                     MessageBoxButtons.OK,
                     MessageBoxIcon.Exclamation);
-
                 return;
             }
 
             TelaItemTemaForm telaItemTema = new(temaSelecionado);
 
-            DialogResult dialogResult = telaItemTema.ShowDialog();
-
-            if (dialogResult == DialogResult.OK)
+            if (telaItemTema.ShowDialog() == DialogResult.OK)
             {
                 List<ItemTema> itensParaAdd = telaItemTema.ObterItemRegistrado();
 
@@ -118,9 +103,9 @@ namespace FestasInfantis.WinApp.ModuloTema
                 }
 
                 _repositorioTema.Editar(temaSelecionado.Id, temaSelecionado);
-
-                CarregarTemas();
             }
+
+            CarregarTemas();
         }
 
         public override void EscolherItens()
@@ -129,30 +114,21 @@ namespace FestasInfantis.WinApp.ModuloTema
 
             if (temaSelecionado == null)
             {
-                MessageBox.Show($"Selecione um tema primeiro!",
+                MessageBox.Show("Selecione um tema primeiro!",
                     "Itens do Tema",
                     MessageBoxButtons.OK,
                     MessageBoxIcon.Exclamation);
-
                 return;
             }
 
             TelaAtualizarItensForm telaAtualizarItens = new(temaSelecionado);
 
-            DialogResult dialogResult = telaAtualizarItens.ShowDialog();
-
-            if (dialogResult == DialogResult.OK)
+            if (telaAtualizarItens.ShowDialog() == DialogResult.OK)
             {
                 List<ItemTema> itensMarcados = telaAtualizarItens.ObterItensMarcados();
-
                 List<ItemTema> itensDesmarcados = telaAtualizarItens.ObterItensDesmarcados();
 
-                Dictionary<ItemTema, bool> estadosAnteriores = new();
-
-                foreach (ItemTema item in temaSelecionado.Itens)
-                {
-                    estadosAnteriores[item] = item.Marcado;
-                }
+                Dictionary<ItemTema, bool> estadosAnteriores = temaSelecionado.Itens.ToDictionary(item => item, item => item.Marcado);
 
                 decimal varAux = 0;
 
@@ -177,9 +153,9 @@ namespace FestasInfantis.WinApp.ModuloTema
                 temaSelecionado.ValorTotal += varAux;
 
                 _repositorioTema.Editar(temaSelecionado.Id, temaSelecionado);
-
-                CarregarTemas();
             }
+
+            CarregarTemas();
         }
 
         public override string ObterTipoCadastro()
@@ -195,18 +171,14 @@ namespace FestasInfantis.WinApp.ModuloTema
         public override UserControl ObterUserControl()
         {
             _tabelaTema ??= new TabelaTemaControl();
-
             CarregarTemas();
-
             return _tabelaTema;
         }
 
         public void CarregarTemas()
         {
             List<Tema> temas = _repositorioTema.SelecionarTodos();
-
             _tabelaTema.AtualizarRegistros(temas);
-
             TelaPrincipalForm.Instancia.AtualizarRodape($"Visualizando {temas.Count} tarefa(s)");
         }
     }
