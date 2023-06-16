7f936e2bd8519d8a913349bf2dc9b047000be147
Rafael Santos <rafaelsantos138@hotmail.com>
2023-06-16 16:32:12 -0300

    corrigido namespace cliente

--- FestasInfantis.Dominio/ModuloCliente/Cliente.cs
+++ FestasInfantis.Dominio/ModuloCliente/Cliente.cs
@@ -1,6 +1,6 @@
 ﻿using System.Text.RegularExpressions;
 
-namespace FestasInfantis.Dominio.ModuloClienteS
+namespace FestasInfantis.Dominio.ModuloCliente
 {
     public class Cliente : EntidadeBase<Cliente>
     {
